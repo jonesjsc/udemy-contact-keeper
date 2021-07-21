583bb23ef376cce030e7e24dfe7ff73af97c26f7
Jason Jones <jason.e.jones@gmail.com>
2021-07-20 23:59:24 -0400

    Section 44 - Create & Respond With JSON Web Token

--- routes/users.js
+++ routes/users.js
@@ -1,6 +1,8 @@
 const express = require('express');
 const router = express.Router();
 const bcrypt = require('bcryptjs');
+const jwt = require('jsonwebtoken');
+const config = require('config');
 const { check, validationResult } = require('express-validator');
 
 const User = require('../models/User');
@@ -44,7 +46,24 @@ router.post(
       const salt = await bcrypt.genSalt(10);
       user.password = await bcrypt.hash(password, salt);
       await user.save();
-      return res.status(201).json({ user });
+
+      const payload = {
+        user: {
+          id: user._id,
+        },
+      };
+
+      jwt.sign(
+        payload,
+        config.get('jwtSecret'),
+        {
+          expiresIn: 36000,
+        },
+        (err, token) => {
+          if (err) throw err;
+          res.status(201).json({ token });
+        }
+      );
     } catch (err) {
       console.error(err.message);
       res.status(500).json({ error: 'Server Error' });
