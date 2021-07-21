b05e40ce2c38390a6eea283b1bdad641a80030ed
Jason Jones <jason.e.jones@gmail.com>
2021-07-20 23:38:24 -0400

    Section 43 - Hash Passwords & Register Route

--- routes/users.js
+++ routes/users.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const router = express.Router();
+const bcrypt = require('bcryptjs');
 const { check, validationResult } = require('express-validator');
 
 const User = require('../models/User');
@@ -9,6 +10,11 @@ const User = require('../models/User');
 // @access Public
 router.post(
   '/',
+  // @validation rules
+  // name - required
+  // email - required
+  // password - required and minLength: 6
+
   [
     check('name', 'Name is required').not().isEmpty(),
     check('email', 'Email is required').isEmail(),
@@ -17,12 +23,32 @@ router.post(
       'Password is required with 6 or more characters'
     ).isLength({ min: 6 }),
   ],
-  (req, res) => {
+
+  async (req, res) => {
     const errors = validationResult(req);
     if (!errors.isEmpty()) {
       return res.status(400).json({ errors: errors.array() });
     }
-    res.send('passed');
+
+    const { name, email, password } = req.body;
+
+    try {
+      let user = await User.findOne({ email });
+
+      if (user) {
+        return res.status(409).json({ error: 'User already exists' });
+      }
+
+      user = new User({ name, email, password });
+
+      const salt = await bcrypt.genSalt(10);
+      user.password = await bcrypt.hash(password, salt);
+      await user.save();
+      return res.status(201).json({ user });
+    } catch (err) {
+      console.error(err.message);
+      res.status(500).json({ error: 'Server Error' });
+    }
   }
 );
 
